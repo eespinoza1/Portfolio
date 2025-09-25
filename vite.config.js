f75be8b6506f91092742f632ce0dafb24a800824
eric <ericksespinoza1@icloud.com>
2025-09-25 13:47:06 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://ericksespinoza.site/"
+  base: '/Portfolio/',  // 👈 usa el nombre exacto de tu repo
 })
